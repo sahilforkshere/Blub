d4b56fc6bd7e3d24c40d84204cf61e28f84029e2
Sahil <paalsahil04@gmail.com>
2025-07-18 00:35:58 +0530

    router setup

---
+++ src/app/(tabs)/communities.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const CommunitiesScren = () => {
+  return (
+    <View>
+      <Text>CommunitiesScren</Text>
+    </View>
+  )
+}
+
+export default CommunitiesScren
