8f32881904460820c740ae7656e397d4a0728d9e
Sahil <paalsahil04@gmail.com>
2025-07-18 01:15:59 +0530

    home

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,7 +1,77 @@
 import { Tabs } from "expo-router";
+import Entypo from "@expo/vector-icons/Entypo";
+import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import Fontisto from "@expo/vector-icons/Fontisto";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 
 export default function TabLayout() {
-    return (
-        <Tabs/>
-    )
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: "#358A92",
+        tabBarInactiveTintColor: "#999",
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          headerTitle: "Blub",
+          headerTintColor: "#358A92",
+          tabBarIcon: ({ color, size }) => (
+            <Entypo name="home" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="communities"
+        options={{
+          title: "Communities",
+          headerTitle: "Blub",
+          headerTintColor: "#358A92",
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome6 name="people-roof" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="create"
+        options={{
+          title: "Create",
+          headerTitle: "Blub",
+          headerTintColor: "#358A92",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="create-outline" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="chat"
+        options={{
+          title: "Chat",
+          headerTitle: "Blub",
+          headerTintColor: "#358A92",
+          tabBarIcon: ({ color, size }) => (
+            <Fontisto name="hipchat" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="inbox"
+        options={{
+          title: "Inbox",
+          headerTitle: "Blub",
+          headerTintColor: "#358A92",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons
+              name="bell-badge"
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+    </Tabs>
+  );
 }
--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,13 +1,48 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-
+import { View, Text, Image, StyleSheet } from "react-native";
+import React from "react";
+import posts from "../../../assets/data/posts.json";
+import { formatDistanceToNowStrict } from "date-fns";
 
 const HomeScreen = () => {
+  const post = posts[0];
   return (
     <View>
-      <Text>HomeScreen</Text>
+      <View style={styles.conatiner}>
+        <Image
+          source={{ uri: post.group.image }}
+          style={{ width: 20, height: 20, borderRadius: 10 }}
+        />
+        <Text style={{ fontWeight: "bold" }}> {post.group.name}</Text>
+        <Text style={{ color: "grey" }}>
+          {formatDistanceToNowStrict(new Date(post.created_at))} ago
+        </Text>
+        <View style={styles.buttonContainer}>
+          <Text style={styles.button}>Join</Text>
+        </View>
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
+
+const styles = StyleSheet.create({
+  conatiner: {
+    flexDirection: "row",
+    gap: 10,
+    paddingHorizontal: 15,
+  },
+
+  buttonContainer: {
+    marginLeft: "auto",
+  },
+  button: {
+    backgroundColor: "#0d469b",
+    color: "white",
+    paddingVertical: 2,
+    paddingHorizontal: 7,
+    borderRadius: 10,
+    fontWeight: "bold",
+    marginRight: 5,
+  },
+});
