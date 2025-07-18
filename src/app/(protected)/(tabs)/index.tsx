5d2effc7b6e33f55be206379ca880fead5e96bf5
Sahil <paalsahil04@gmail.com>
2025-07-18 12:20:10 +0530

    clerk

--- src/app/(protected)/(tabs)/index.tsx
+++ src/app/(protected)/(tabs)/index.tsx
@@ -1,12 +1,12 @@
 import { View, Text, Image, StyleSheet, TouchableOpacity } from "react-native";
 import React from "react";
-import posts from "../../../assets/data/posts.json";
+import posts from "../../../../assets/data/posts.json";
 import { formatDistanceToNowStrict } from "date-fns";
 import { ImageSourcePropType } from "react-native";
 import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import AntDesign from "@expo/vector-icons/AntDesign";
-import PostListItem from "../../components/PostListItem";
+import PostListItem from "../../../components/PostListItem";
 import { FlatList } from "react-native";
 
 
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,11 +1,17 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { Slot } from 'expo-router'
+import { View, Text } from "react-native";
+import React from "react";
+import { Slot } from "expo-router";
+import { ClerkLoaded, ClerkProvider } from "@clerk/clerk-expo";
+const key = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
 
 const RootLayout = () => {
-  return <Slot/>
-  
-  
-}
+  return (
+    <ClerkProvider publishableKey={key}>
+      <ClerkLoaded>
+        <Slot />
+      </ClerkLoaded>
+    </ClerkProvider>
+  );
+};
 
-export default RootLayout
+export default RootLayout;
